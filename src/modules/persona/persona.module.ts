3e739be55782479c7c2978d7f702dda2ed9a0c17
Mike A <mike.anderson.balt@gmail.com>
2022-06-14 17:45:44 -0400

    fixes cloud build failure

--- src/modules/persona/persona.module.ts
+++ src/modules/persona/persona.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PersonaService } from './persona.service';
+import { PrismaService } from 'src/common/prisma.service';
 
 @Module({
-  providers: [PersonaService],
+  providers: [PersonaService, PrismaService],
 })
 export class PersonaModule {}
