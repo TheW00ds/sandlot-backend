083ea4b54edd04b0adbf2e6872aaaf539fe0c741
Mike Anderson <105335449+TheW00ds@users.noreply.github.com>
2022-06-14 18:38:47 -0400

    Revert "9 cloud build failure (#12)"
    This reverts commit 85b53ea008b9eee7618d5262ae5e8bb9fdd80e85.

--- src/modules/persona/persona.module.ts
+++ src/modules/persona/persona.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PersonaService } from './persona.service';
-import { PrismaService } from 'src/common/prisma.service';
 
 @Module({
-  providers: [PersonaService, PrismaService],
+  providers: [PersonaService],
 })
 export class PersonaModule {}
