c68fe56447c353a60ada20befce3e74d56e1fc42
Mike A <mike.anderson.balt@gmail.com>
2022-06-14 14:32:33 -0400

    adds shallow crud methods to ride service

--- src/modules/ride/ride.service.ts
+++ src/modules/ride/ride.service.ts
@@ -5,4 +5,54 @@ import { Ride, Prisma } from '@prisma/client';
 @Injectable()
 export class RideService {
   constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.RideCreateInput): Promise<Ride> {
+    return this.prisma.ride.create({ data });
+  }
+
+  async findAll(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.RideWhereUniqueInput;
+    where?: Prisma.RideWhereInput;
+    orderBy?: Prisma.RideOrderByWithRelationInput;
+  }): Promise<Ride[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.ride.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async find(
+    rideWhereUniqueInput: Prisma.RideWhereUniqueInput,
+  ): Promise<Ride | null> {
+    return this.prisma.ride
+      .findUnique({
+        where: rideWhereUniqueInput,
+      })
+      .then((data): Ride | null => {
+        return data;
+      });
+  }
+
+  async update(params: {
+    where: Prisma.RideWhereUniqueInput;
+    data: Prisma.RideUpdateInput;
+  }): Promise<Ride> {
+    const { where, data } = params;
+    return this.prisma.ride.update({
+      data,
+      where,
+    });
+  }
+
+  async delete(where: Prisma.RideWhereUniqueInput): Promise<Ride> {
+    return this.prisma.ride.delete({
+      where,
+    });
+  }
 }
