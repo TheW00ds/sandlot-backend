831e2e32d2ddbcdf4d66758d292795ca20c92e95
Mike A <mike.anderson.balt@gmail.com>
2022-06-14 14:51:22 -0400

    adds shallow crud methods to groupMembership svc

--- src/modules/group-membership/group-membership.service.ts
+++ src/modules/group-membership/group-membership.service.ts
@@ -5,4 +5,58 @@ import { GroupMembership, Prisma } from '@prisma/client';
 @Injectable()
 export class GroupMembershipService {
   constructor(private prisma: PrismaService) {}
+
+  async create(
+    data: Prisma.GroupMembershipCreateInput,
+  ): Promise<GroupMembership> {
+    return this.prisma.groupMembership.create({ data });
+  }
+
+  async findAll(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.GroupMembershipWhereUniqueInput;
+    where?: Prisma.GroupMembershipWhereInput;
+    orderBy?: Prisma.GroupMembershipOrderByWithRelationInput;
+  }): Promise<GroupMembership[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.groupMembership.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async find(
+    groupMembershipWhereUniqueInput: Prisma.GroupMembershipWhereUniqueInput,
+  ): Promise<GroupMembership | null> {
+    return this.prisma.groupMembership
+      .findUnique({
+        where: groupMembershipWhereUniqueInput,
+      })
+      .then((data): GroupMembership | null => {
+        return data;
+      });
+  }
+
+  async update(params: {
+    where: Prisma.GroupMembershipWhereUniqueInput;
+    data: Prisma.GroupMembershipUpdateInput;
+  }): Promise<GroupMembership> {
+    const { where, data } = params;
+    return this.prisma.groupMembership.update({
+      data,
+      where,
+    });
+  }
+
+  async delete(
+    where: Prisma.GroupMembershipWhereUniqueInput,
+  ): Promise<GroupMembership> {
+    return this.prisma.groupMembership.delete({
+      where,
+    });
+  }
 }
